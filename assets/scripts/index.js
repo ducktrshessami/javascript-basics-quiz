ee47b4ea26f5e36397fa4b7df6d1d28f0313a041
ducktrshessami <jwee761@gmail.com>
2020-10-19 17:09:40 -0700

    question display

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,6 @@
 // Define vars
-const questions = [
+var mainEl = document.querySelector("main");
+var questions = [ // A proper quiz wouldn't have the answers in plaintext >.>
     {
         question: "Commonly used data types DO NOT include:",
         choices: [
@@ -8,7 +9,7 @@ const questions = [
             "alerts",
             "numbers"
         ],
-        answer: 2
+        answer: 3
     }
 ];
 
@@ -16,31 +17,93 @@ const questions = [
 Clear all content inside the main tag
 */
 function clearContent() {
-    document.querySelector("main").innerHTML = "";
+    mainEl.innerHTML = "";
 }
 
 /*
 Change page structure for question screen
 */
 function questionPage() {
-    ;
+    let section, questionEl, divider, choiceList, message;
+    
+    clearContent();
+
+    // Content section
+    section = document.createElement("section");
+    section.id = "question-page";
+    section.className = "col-md-6 m-auto";
+
+    // Question header
+    questionEl = document.createElement("h1");
+    questionEl.id = "question";
+
+    // Line break
+    divider = document.createElement("br");
+
+    // Choice list buttons
+    choiceList = document.createElement("ul");
+    choiceList.id = "choice-list";
+
+    // Answer validation message
+    message = document.createElement("span");
+    message.id = "msg";
+    message.style.display = "none";
+
+    // Append to parents
+    mainEl.appendChild(section);
+    section.appendChild(questionEl);
+    section.appendChild(divider);
+    section.appendChild(choiceList);
+    section.appendChild(message);
 }
 
 /*
-Display a question on the question screen
+Display a question
 'questions' constant assumed to be defined
 @params:
 i - the index of the question in the 'questions' constant
 */
 function displayQuestion(i) {
-    ;
+    let questionEl, choiceList;
+
+    if (!document.querySelector("#question-page")) { // Page layout
+        questionPage();
+    }
+
+    // Get elements
+    questionEl = document.querySelector("#question");
+    choiceList = document.querySelector("#choice-list");
+
+    // Fill layout with content
+    questionEl.textContent = questions[i].question;
+    choiceList.innerHTML = ""; // Clear previous choices
+    for (let j = 0; j < questions[i].choices.length; j++) {
+        let choiceEl, choiceBtn;
+
+        // Create choice elements
+        choiceEl = document.createElement("li");
+        choiceBtn = document.createElement("button");
+
+        // Button properties
+        choiceBtn.className = "btn";
+        choiceBtn.setAttribute("data-value", j + 1);
+        choiceBtn.textContent = (j + 1) + ". " + questions[i].choices[j];
+
+        // Append to parents
+        choiceList.appendChild(choiceEl);
+        choiceEl.appendChild(choiceBtn);
+    }
+
+    // Listen for choice
+    choiceList.addEventListener("click", choiceSelect);
 }
 
 /*
 Start timer and display question screen
 */
 function startQuiz() {
-    clearContent();
+    questionPage();
+    displayQuestion(0);
 }
 
 /*
@@ -50,7 +113,7 @@ event - click event
 */
 function choiceSelect(event) {
     if (event.target.matches("button")) {
-        ;
+        console.log(event.target.getAttribute("data-value"));
     }
 }
 
