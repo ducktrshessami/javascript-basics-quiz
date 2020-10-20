3a7f4128389ce4167377bb50cd9e4b8de97a088c
ducktrshessami <jwee761@gmail.com>
2020-10-19 17:47:22 -0700

    more questions

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -11,6 +11,46 @@ var questions = [ // A proper quiz wouldn't have the answers in plaintext >.>
             "numbers"
         ],
         answer: 3
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within _____.",
+        choices: [
+            "quotes",
+            "curly brackets",
+            "parentheses",
+            "square brackets"
+        ],
+        answer: 3
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _____.",
+        choices: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above"
+        ],
+        answer: 4
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        choices: [
+            "commas",
+            "curly brackets",
+            "quotes",
+            "parentheses"
+        ],
+        answer: 3
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: [
+            "JavaScript",
+            "terminal / bash",
+            "for loops",
+            "console.log"
+        ],
+        answer: 4
     }
 ];
 
